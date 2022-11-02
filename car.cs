7ab980df5c2e48e70b942ce56691379838ac6203
Sebastian Thomsen <sebastianlundthomsen@gmail.com>
2022-11-02 09:48:04 +0100

    Lavede en car og lidt mere

---
+++ car.cs
@@ -0,0 +1,13 @@
+class car
+{
+    public list<door> doors;
+    public float price = 0f;
+    public float speed = 0f;
+    public car(string s, float money){
+
+        doors = new list<door>();
+        for(int i = 0; i < 4; i++){
+            
+        }
+    }
+}
